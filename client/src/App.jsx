ce27661337e85f71116b7d8bfc1757be1d718e78
Duy Tran <duytran_d@yahoo.com>
2024-08-16 18:31:41 -0500

    make webap

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   const [array, setArray] = useState([]);
 
   const fetchAPI = async () => {
-    const response = await axios.get(import.meta.env.VITE_API_URL);
+    const response = await axios.get("http://duyviteserver.azurewebsites.net:8080/api");
     setArray(response.data.fruits);
     console.log(response.data.fruits);
   };
